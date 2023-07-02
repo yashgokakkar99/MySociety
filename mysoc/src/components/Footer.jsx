0b74e44449ddc64a78affb89762b135acea649c9
Yash Gokakkar <yashgokakkar@gmail.com>
2023-07-02 18:56:16 +0530

    Navbar updated

--- mysoc/src/components/Footer.jsx
+++ mysoc/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="fixed bottom-0 w-full bg-red-900 text-white h-15">Footer</div>
+    <div className="fixed bottom-0 w-full bg-red-400 text-white h-18 flex justify-center text-red-900 font-bold">Copyright © 2023 mysoc | All rights reserved</div>
   )
 }
 
--- mysoc/src/components/Navbar.jsx
+++ mysoc/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="sticky bg-red-400 fixed h-20 w-full -top-10">
-        <Link classNmae="menu" to={"/"}>Home</Link>
-        <Link classNmae="menu" to={"/society"}>Society</Link>
-        <Link classNmae="menu" to={"/about"}>About</Link>
-        <Link classNmae="menu" to={"/contact"}>Contact</Link>
+    <div className="sticky bg-red-400 fixed  w-full space-x-16 px-10 flex item-center justify-end py-4">
+      <Link className="menu border-2 p-2 hover:bg-red-200 rounded-lg hover:font-bold hover:text-red-700 border-red-900 w-24 flex justify-center" to={"/"}>
+        Home
+      </Link>
+      <Link className="menu border-2 p-2 hover:bg-red-200 rounded-lg hover:font-bold hover:text-red-700 border-red-900 w-24 flex justify-center" to={"/society"}>
+        Society
+      </Link>
+      <Link className="menu border-2 p-2 hover:bg-red-200 rounded-lg hover:font-bold hover:text-red-700 border-red-900 w-24 flex justify-center" to={"/about"}>
+        About
+      </Link>
+      <Link className="menu border-2 p-2 hover:bg-red-200 rounded-lg hover:font-bold hover:text-red-700 border-red-900 w-24 flex justify-center" to={"/contact"}>
+        Contact
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
